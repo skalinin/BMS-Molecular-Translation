57e99887b6cfcf5d312960c1cb5ff61fce757440
Stanislav Kalinin <kalinin@sent.com>
2021-04-01 10:01:11 +0300

    feat: Add load_pretrain_model func

--- src/bms/utils.py
+++ src/bms/utils.py
@@ -1,3 +1,19 @@
+import torch
+
+
+def load_pretrain_model(weights_path, model, device):
+    old_model = torch.load(weights_path, device)
+    new_dict = model.state_dict()
+    old_dict = old_model
+    for key, weights in new_dict.items():
+        if key in old_dict:
+            if new_dict[key].shape == old_dict[key].shape:
+                new_dict[key] = old_dict[key]
+            else:
+                print('\n Weights {} were not loaded'.format(key))
+        else:
+            print('\n Weights {} were not loaded'.format(key))
+    return new_dict
 
 
 def get_file_path(image_id, main_folder='train'):
